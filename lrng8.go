e6e1ad74c18268292d5875fbd855aa91fd60c998
sagethor <thelittlesage@gmail.com>
2021-12-21 10:07:19 -0500

    Added main.go for execution, lrng8.go works!

--- lrng8.go
+++ lrng8.go
@@ -1,22 +1,32 @@
-var buffC uint8;
-var buff [256]byte;
+package main
+
+import "time"
+import "encoding/binary"
+import "math/bits"
+// hacky - run lrng8() once to fill buff on init
+var buffC uint8 = 255;
+var buff [256]uint8;
+
+func nanow() int64 {
+	return time.Now().UnixNano();
+}
 
 func lrng8() uint8 {
 	if (buffC == 255) {
-		if (bitsUintSize == 64) {
+		if (bits.UintSize == 64) {
 			defer lrng8_64();
 		} else {
 			defer lrng8_32();
 		}
 	}
-		return buff[buffC];
+	return buff[buffC];
 }
 
 var seed32 uint32 = uint32(nanow());
 func lrng8_32() {
 	_ = buff[255];
 	for i := 0; i < 64; i++ {
-		seed *= 69069;
+		seed32 *= 69069;
 		seed32++;
 		binary.LittleEndian.PutUint32(buff[i*4:], seed32);
 	}
@@ -25,7 +35,7 @@ func lrng8_32() {
 var seed64 uint64 = uint64(nanow());
 func lrng8_64() {
 	_ = buff[255];
-	for i := 0; i < 32; i++ {
+	for j := 0; j < 32; j++ {
 		seed64 *= 6364136223846793005;
 		seed64++;
 		binary.LittleEndian.PutUint64(buff[j*8:], seed64);
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	lrng8();
+	fmt.Println(lrng8());
+}
