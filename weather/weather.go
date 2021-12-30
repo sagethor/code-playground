83fab19d49987f4c1c7cc10a43bb054e4d77c229
sagethor <thelittlesage@gmail.com>
2021-12-29 21:00:00 -0500

    Pseudocode for weather... need to develop state machine model.

--- weather/weather.go
+++ weather/weather.go
@@ -9,3 +9,14 @@ func phase() int64 {
 	return time.Now().Unix() / 25200;
 }
 
+// rough boilerplate for actual implementation
+// assuming worldwrap & value wraparound works ok
+func simulate(x, y, z uint8) uint8 {
+	zone uint32 = x + y*256 + z*65536;
+	forecast := getWeather(zone);
+	// add function to decode conditions from forecast?
+
+	humidity, temperature, wind uint8;
+	// randomly modify one of the attributes (or not at all) to see next weather pattern?
+}
+
