ea7ed1555b7f6f3cd1ae6e696849025269befc35
sagethor <thelittlesage@gmail.com>
2022-01-11 17:34:45 -0500

    Starting simpler with the scraper...

--- caek-scraper/main.go
+++ caek-scraper/main.go
@@ -2,63 +2,25 @@ package main
 
 import (
 	"github.com/gocolly/colly/v2"
-	//"time"
+	"time"
 	"fmt"
-	"log"
 )
 
 func main() {
-	// initialize new colly Collector
-	c := colly.NewCollector(
-		// Restrict crawling to specific domains
-		// colly.AllowedDomains(),
-		// Allow visiting the same page multiple times
-		colly.AllowURLRevisit(),
-		// Allow crawling to be done in parallel / async
-		colly.Async(true),
-	);
+	c := colly.NewCollector();
 
-	/*
-	c.Limit(&colly.LimitRule{
-		// Filter domains affected by this rule
-		// DomainGlob: "",
-		// Set a delay between requests to these domains
-		Delay: 1 * time.Second,
-		// Add an additional random delay
-		RandomDelay: 1 * time.Second,
-	});
-	*/
-
-
-	// add callbacks to Collector
+	c.SetRequestTimeout(120 * time.Second);
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL);
 	});
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err);
-	});
-
-	c.OnResponseHeaders(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL);
-	});
-
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL);
+		fmt.Println("Got a response from", r.Request.URL);
 	});
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"));
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Println("Got this error:", e);
 	});
 
-	c.OnXML("//h1", func(e *colly.XMLElement) {
-		fmt.Println(e.Text);
-	});
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL);
-	});
-
-
-	c.Visit("https://old.reddit.com/r/popular/?count=100");
+	c.Visit("https://www.youtube.com/");
 }
